1c17653409b631ee1f0b41b9012331f7352efa11
Noor Alhuda-Odat <nooralhuda.mohammad.odat@gmail.com>
2025-05-10 14:58:50 +0300

    update 2

--- src/test/java/Almosafer/Almosafer/AppTest.java
+++ src/test/java/Almosafer/Almosafer/AppTest.java
@@ -16,148 +16,121 @@ import org.testng.annotations.Test;
 public class AppTest extends TestData {
 
 	@BeforeTest
-	public void mySetup() throws InterruptedException {
+	public void mySetup(){
+		
 		Setup();
 	}
 	
-	@Test(priority = 1)
+	@Test(priority = 1, enabled = false)
 	public void ChekTheDefultLanguages () {
-	
+		
 		String ActualLanguage= driver.findElement(By.tagName("html")).getDomAttribute("lang");
-        Assert.assertEquals(ActualLanguage, ExpectLanguage, "Language should be EN by default.");
-  
+		Assert.assertEquals(ActualLanguage, ExpectLanguage, "Language should be EN by default.");
 	}
 	
-	@Test(priority = 2 )
+	@Test(priority = 2, enabled = false)
 	public void  ChekTheCurrency() {
-	
+		
 		String ActualCurrency =driver.findElement(By.cssSelector(".sc-hUfwpO.kAhsZG")).getText();
-        Assert.assertEquals(ActualCurrency, ExpectCurrency, "Currency should be SAR.");
-        
+		Assert.assertEquals(ActualCurrency, ExpectCurrency, "Currency should be SAR.");
 	}
 	
-	@Test(priority = 3)
+	@Test(priority = 3, enabled = false)
 	public void  ChekTheContactNumber() {
 		
 		String ActualContactNumber =driver.findElement(By.linkText("+966554400000")).getText();
 		Assert.assertEquals(ActualContactNumber, ExpectContactNumber, "Saudi contact number (" + ExpectContactNumber + ") should be visible on the page.");
-	
 	}
 	
-	@Test(priority = 4)
+	@Test(priority = 4, enabled = false)
 	public void checkQitafLogoIsDisplayed() throws InterruptedException {
 		
-	    WebElement Footer = driver.findElement(By.tagName("footer"));
-	    WebElement QitafLogo = Footer.findElement(By.cssSelector(".sc-ekulBa.iOOTo")).findElement(By.tagName("svg"));
-	    boolean ActualResultForThelogo = QitafLogo.isDisplayed();
+		WebElement Footer = driver.findElement(By.tagName("footer"));
+		WebElement QitafLogo = Footer.findElement(By.cssSelector(".sc-ekulBa.iOOTo")).findElement(By.tagName("svg"));
+		boolean ActualResultForThelogo = QitafLogo.isDisplayed();
 		Assert.assertEquals(ActualResultForThelogo, ExpectedResultsForTheLogo,"Qitaf Logo is displayed successfully.");	
-	
 	}
 	
-	@Test(priority = 5)
+	@Test(priority = 5, enabled = false)
 	public void verifyHotelsTabIsNotSelected() {
 		
-	    WebElement HotelsTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
-	    String ActualValueForHotelTab = HotelsTab.getDomAttribute("aria-selected");
-	    Assert.assertEquals(ActualValueForHotelTab, ExpectedValueForHotelTab, "Hotels tab should NOT be selected by default.");
-	    
+		WebElement HotelsTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		String ActualValueForHotelTab = HotelsTab.getDomAttribute("aria-selected");
+		Assert.assertEquals(ActualValueForHotelTab, ExpectedValueForHotelTab, "Hotels tab should NOT be selected by default.");    
 	}
 	
-	@Test(priority = 6)
+	@Test(priority = 6, enabled = false)
 	public void verifyDefaultDepartureDate() {
 		
 		List<WebElement> Dates = driver.findElements(By.cssSelector(".sc-dXfzlN.iPVuSG"));
 		String ActualDepatureDate = Dates.get(0).getText();
-		Assert.assertEquals(ActualDepatureDate, ExpectedDepatureDate);
-		
+		Assert.assertEquals(ActualDepatureDate, ExpectedDepatureDate);	
 	}
 
-	@Test(priority = 7)
+	@Test(priority = 7, enabled = false)
 	public void verifyDefaultReturnDate() {
 		
 		List<WebElement> Dates = driver.findElements(By.cssSelector(".sc-dXfzlN.iPVuSG"));
 		String ActualReturnDate = Dates.get(1).getText();
-		Assert.assertEquals(ActualReturnDate, ExpectedReturnDate);
-		
+		Assert.assertEquals(ActualReturnDate, ExpectedReturnDate);	
 	}
 	
-	@Test(priority = 8)
+	@Test(priority = 8, enabled = false)
 	public void RandomlyChangeTheLanguage() {
 		
 		driver.get(URLs[RandomIndex]);
-		
 	      if (driver.getCurrentUrl().contains("en")) {
-			  String ActualLanaguge = driver.findElement(By.tagName("html")).getDomAttribute("lang");
-			  Assert.assertEquals(ActualLanaguge, ExpectedLanaguageEn);
-	        } 
-		  else {
-			    String ActualLanaguge = driver.findElement(By.tagName("html")).getDomAttribute("lang");
-				Assert.assertEquals(ActualLanaguge, ExpectedLanaguageAr);
-	        } 
-		
+	    	  String ActualLanaguge = driver.findElement(By.tagName("html")).getDomAttribute("lang");
+	    	  Assert.assertEquals(ActualLanaguge, ExpectedLanaguageEn); 
+	      }
+	      else {
+	    	  String ActualLanaguge = driver.findElement(By.tagName("html")).getDomAttribute("lang");
+	    	  Assert.assertEquals(ActualLanaguge, ExpectedLanaguageAr);
+	      } 
 	}
 
-	@Test(priority = 9)
+	@Test(priority = 9, enabled = false)
 	public void clickCities() throws InterruptedException {
 		
-		 StaysButton();
-	 
+		StaysButton();
 		WebElement locationInput = driver.findElement(By.xpath("//input[@data-testid='AutoCompleteInput']"));
-		
-		  if (driver.getCurrentUrl().contains("en")) {
-			  locationInput.sendKeys(RandomEnglishCities);
-		    } 
-		  else {
-		        locationInput.sendKeys(RandomArabicCities);
-		    }
-	  
-		// firstSuggestion
-		WebElement UnorderedList = driver.findElement(By.cssSelector(".sc-phbroq-4.gGwzVo.AutoComplete__List"));	 
-		UnorderedList.findElements(By.tagName("li")).get(1).click();
-			  
+	      if (driver.getCurrentUrl().contains("en")) {
+	    	  locationInput.sendKeys(RandomEnglishCities);
+	      } 
+	      else {
+	    	  locationInput.sendKeys(RandomArabicCities);
+	      }
+	      // firstSuggestion
+	      WebElement UnorderedList = driver.findElement(By.cssSelector(".sc-phbroq-4.gGwzVo.AutoComplete__List"));
+	      UnorderedList.findElements(By.tagName("li")).get(1).click();	  
 	}
 	
-	@Test(priority = 10)
+	@Test(priority = 10, enabled = false)
 	public void selectRandomRoomOption() throws InterruptedException {
 		
 		//StaysButton();
-		
 		WebElement RoomSelectElement = driver.findElement(By.xpath("//select[@data-testid='HotelSearchBox__ReservationSelect_Select']"));
 		Select MySelect = new Select(RoomSelectElement);
 		int RandomDropdownIndex = rand.nextInt(2);
 		MySelect.selectByIndex(RandomDropdownIndex);
 	}
 	
-	@Test(priority = 11)
+	@Test(priority = 11, enabled = false)
 	public void clickSearchHotelsButton() throws InterruptedException {
 		
 		//StaysButton();
-		
 		WebElement searchButton = driver.findElement(By.xpath("//button[@data-testid='HotelSearchBox__SearchButton']"));
 		searchButton.click();
 	}
 	
-	@Test(priority = 12)
+	@Test(priority = 12, enabled = false)
 	public void NewPage() throws InterruptedException {
 		
 		WebDriverWait Wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-		 
-        WebElement SearchResult = Wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-testid='srp_properties_found']")));
-
-        String ResultText = SearchResult.getText();
-        System.out.println(ResultText);
- 
-        boolean ActualResult = ResultText.contains("found") || ResultText.contains("مكان");
- 
-        Assert.assertEquals(ActualResult, ExpectedFinshingSearchHotel);
-        
-     /*or
-        Get and print the readyState
-     	String pageState = (String) js.executeScript("return document.readyState");
-        System.out.println(pageState);
-        assertEquals(pageState, "complete");
-    
-     */
-    }
-	
+		WebElement SearchResult = Wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-testid='srp_properties_found']")));
+		String ResultText = SearchResult.getText();
+		System.out.println(ResultText);
+		boolean ActualResult = ResultText.contains("found") || ResultText.contains("مكان");
+		Assert.assertEquals(ActualResult, ExpectedFinshingSearchHotel);
+	}
 }
--- src/test/java/Almosafer/Almosafer/TestData.java
+++ src/test/java/Almosafer/Almosafer/TestData.java
@@ -9,56 +9,51 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestData {
-	
 	WebDriver driver = new ChromeDriver();
 	String URL ="https://global.almosafer.com/en";
 	Random rand = new Random();
 	
-	
 //for test 1
- String ExpectLanguage = "en" ;
+	String ExpectLanguage = "en" ;
  
 //for test 2
-  String ExpectCurrency ="SAR";
+	String ExpectCurrency ="SAR";
 
 //for test 3
-  String ExpectContactNumber ="+966554400000" ;
+	String ExpectContactNumber ="+966554400000" ;
 
 //for test 4
-  boolean ExpectedResultsForTheLogo = true;
+	boolean ExpectedResultsForTheLogo = true;
  
 //for test 5
-  String ExpectedValueForHotelTab = "false";
+	String ExpectedValueForHotelTab = "false";
  
 
 //for test 6+7
-  LocalDate Today = LocalDate.now();
- 
+	LocalDate Today = LocalDate.now();
 	int Tomorrow = Today.plusDays(1).getDayOfMonth();
 	String ExpectedDepatureDate = String.format("%02d", Tomorrow);
-	
 	int TheDayAfterTomorrow = Today.plusDays(2).getDayOfMonth();
 	String ExpectedReturnDate = String.format("%02d", TheDayAfterTomorrow);
 	
 //for test 8
-  String[] URLs = { "https://www.almosafer.com/en", "https://www.almosafer.com/ar", };
-  int RandomIndex = rand.nextInt(URLs.length);
-  String ExpectedLanaguageEn = "en";
-  String ExpectedLanaguageAr = "ar";
+	String[] URLs = { "https://www.almosafer.com/en", "https://www.almosafer.com/ar", };
+	int RandomIndex = rand.nextInt(URLs.length);
+	String ExpectedLanaguageEn = "en";
+	String ExpectedLanaguageAr = "ar";
 	
 //for test 9
-  String[] EnglishCities = { "Dubai", "Jeddah", "Riyadh" };
-  int RandomIndex1 = rand.nextInt(EnglishCities.length);
-  String RandomEnglishCities =EnglishCities[RandomIndex1];
-
-  String[] ArabicCities = { "دبي", "جدة" };
-  int RandomIndex2 = rand.nextInt(ArabicCities.length);
-  String RandomArabicCities =ArabicCities[RandomIndex2];
+	String[] EnglishCities = { "Dubai", "Jeddah", "Riyadh" };
+	int RandomIndex1 = rand.nextInt(EnglishCities.length);
+	String RandomEnglishCities =EnglishCities[RandomIndex1];
+	String[] ArabicCities = { "دبي", "جدة" };
+	int RandomIndex2 = rand.nextInt(ArabicCities.length);
+	String RandomArabicCities =ArabicCities[RandomIndex2];
 
 //for test 9/10/11 if you want to run 10 or 11 alone you should remove the comment from it 
   public void StaysButton() throws InterruptedException {
-	WebElement hotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
-	hotelTab.click();
+	  WebElement hotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+	  hotelTab.click();
 	}
 
 //for test 12
@@ -66,10 +61,10 @@ public class TestData {
 
 //for BeforeTest 
   public void Setup() {
-	driver.get(URL);
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3)); 	
-	driver.findElement(By.cssSelector(".cta__button.cta__saudi")).click();
+	  driver.get(URL);
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+	  driver.findElement(By.cssSelector(".cta__button.cta__saudi")).click();
 	}
 
 }
